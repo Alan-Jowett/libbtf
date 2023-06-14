71283f754a197c32dd04536d30947682f97b88f3
Alan Jowett <atjowet@gmail.com>
2023-06-14 16:07:05 -0700

    libfuzzer for libbtf
    Signed-off-by: Alan Jowett <atjowet@gmail.com>

--- libfuzzer/libfuzz_harness.cpp
+++ libfuzzer/libfuzz_harness.cpp
@@ -1,17 +1,39 @@
 // Copyright (c) Microsoft Corporation
 // SPDX-License-Identifier: MIT
 
-#ifdef __cplusplus
-#define FUZZ_EXPORT extern "C" __declspec(dllexport)
-#else
-#define FUZZ_EXPORT __declspec(dllexport)
-#endif
+#include "btf.h"
+#include "btf_json.h"
+#include "btf_map.h"
+#include "btf_parse.h"
+#include "btf_type_data.h"
+#include "btf_write.h"
+#include "elfio/elfio.hpp"
 
-#include <cstdint>
+#include <sstream>
+#include <stddef.h>
+#include <stdint.h>
+#include <string>
 
-FUZZ_EXPORT int __cdecl LLVMFuzzerInitialize(int*, char***) { return 0; }
+extern "C" int LLVMFuzzerInitialize(int *, char ***) { return 0; }
 
-FUZZ_EXPORT int __cdecl LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
-{
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  ELFIO::elfio reader;
+  if (!reader.load(argv[1])) {
+    return 1;
+  }
 
+  auto btf = reader.sections[".BTF"];
+  if (!btf) {
+    return 1;
+  }
+
+  try {
+    libbtf::btf_type_data btf_data = std::vector<std::byte>(
+        {reinterpret_cast<const std::byte *>(data),
+         reinterpret_cast<const std::byte *>(data + size)});
+  } catch (const std::runtime_error &) {
+    return 1;
+  }
+
+  return 0;
 }
